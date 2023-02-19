faff4e7a4749c5e4f2b3380139d2490673fd5d51
thetechgirlgita <thetechgirlgita@gmail.com>
2023-02-19 17:00:14 +0530

    Adding gradient text

--- lib/checkBox.dart
+++ lib/checkBox.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: camel_case_types
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
 
 class checkBox extends StatefulWidget {
   const checkBox({super.key});
@@ -14,15 +16,35 @@ class _checkBoxState extends State<checkBox> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Checkbox(
-        value: isClick,
-        onChanged: (bool? value) {
-          setState(() {
-            isClick = value!;
-          });
-        },
+        body: Column(children: [
+      Padding(
+        padding: const EdgeInsets.all(50),
+        child: GradientText(
+          'Check Box Demo',
+          style: const TextStyle(
+            fontSize: 50,
+            fontWeight: FontWeight.bold,
+          ),
+          // ignore: prefer_const_literals_to_create_immutables
+          colors: [
+            Colors.pink,
+            Colors.yellow,
+          ],
+        ),
       ),
-    ));
+      Padding(
+        padding: EdgeInsets.only(top: 50),
+        child: Checkbox(
+          value: isClick,
+          onChanged: (bool? value) {
+            setState(() {
+              print('clicked');
+              isClick = value!;
+            });
+          },
+          activeColor: Colors.blue,
+        ),
+      )
+    ]));
   }
 }
